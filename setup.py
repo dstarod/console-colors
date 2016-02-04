85fc68e2a2808b68c9b22b701d39a52a836cc056
dstarodubtsev <dstarodubtsev@itrend.tv>
2016-02-04 12:14:50 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='ConsoleColors',
+      version='1.0',
+      description='Make python logs colorized',
+      author='Dmitry Starodubtsec',
+      author_email='dmitry.starodubcev@gmail.com',
+      url='https://dstarod.blogspot.com',
+      packages=['colors', 'colors.color'],
+     )
